93b5155959eb0ce0720b7787ce83cd3441c78ad8
Hrishi Hiraskar <hrishihiraskar@gmail.com>
2018-05-04 12:33:26 +0000

    Message is received by Tracker now

--- addon.cpp
+++ addon.cpp
@@ -13,14 +13,14 @@
 #include "ShowWindow.h"
 
 extern "C" {
-	void populate_menu (BMessage *msg, BMenu* menu);
+	void populate_menu (BMessage* msg, BMenu* menu, BHandler* handler);
 	void message_received (entry_ref dir_ref, BMessage* msg);
 }
 
 const char* ADDON_NAME = "MyAddOn";
 
 void 
-process_refs (entry_ref dir_ref, BMessage *msg, void*)
+process_refs (entry_ref dir_ref, BMessage* msg, void*)
 {
 	BString buffer("Hello World");
 	BAlert *alert = new BAlert("", buffer.String(), "Cancel", 
@@ -30,7 +30,7 @@ process_refs (entry_ref dir_ref, BMessage *msg, void*)
 
 
 void
-populate_menu (BMessage *msg, BMenu* menu)
+populate_menu (BMessage* msg, BMenu* menu, BHandler* handler)
 {
 	if (menu == NULL){
 		BString buffer("Null menu");
@@ -39,7 +39,7 @@ populate_menu (BMessage *msg, BMenu* menu)
 		alert->Go();
 		return;
 	}
-	printf("into populate_menu");
+	printf("into populate_menu\n");
 
 	BMenuItem* item = menu->FindItem(ADDON_NAME);
 	if (item != NULL)
@@ -50,10 +50,11 @@ populate_menu (BMessage *msg, BMenu* menu)
 	BMessage* itemMsg = new BMessage(*msg);
 	itemMsg->AddInt32("addon_item_id", kShowWindow);
 	BMenuItem *menuitem = new BMenuItem("hello", itemMsg);
-	submenu->AddItem(menuitem);
-	// submenu->SetTargetForItems(menu);
 
+	submenu->AddItem(menuitem);
 	menu->AddItem(submenu);
+
+	submenu->SetTargetForItems(handler);
 }
 
 
